55959c4896ed9f819c33c9cd28a73734763d6cd0
gsilva <gsilva@ipinnovatech.co>
2021-09-05 12:00:11 -0500

    Addition of validations and better visualization of results info

--- look4book/forms.py
+++ look4book/forms.py
@@ -2,16 +2,10 @@ from django import forms
 
 class FormsBook(forms.Form):
     
-    name_opt = forms.BooleanField(initial=True, required=False, label='By Name')
+    book_opt = forms.BooleanField(initial=True, required=False, label='By Name')
     author_opt = forms.BooleanField(initial=False, required=False, label='By Author')
     # options = forms.MultipleChoiceField(label='some label',  choices=(('happy','Happy'),('sad','Sad')),
     #   widget=forms.CheckboxSelectMultiple(attrs={'checked' : 'checked'}))
     
-    book_name = forms.CharField(initial="The lord of the rings", required=True, label="Write the book's name separated by pipe '|'")
+    book_name = forms.CharField(required=False, initial="The lord of the rings", label="Write the book's name separated by pipe '|'")
     author_name = forms.CharField(required=False, label="Write the author's name separated by pipe '|'")
-    
-    def clean_terms(self):
-         if self.cleaned_data["name_opt"] == True:
-             raise forms.ValidationError(
-                 "You have to accept terms&conditions to complete registration"
-             )
--- look4book/views.py
+++ look4book/views.py
@@ -3,7 +3,7 @@ from . import services
 from .forms import FormsBook
 from django.contrib import messages
 
-books_results = [{'title':'El señor de los anillos'}]
+books_results = [{'title':'El señor de los anillos', 'authors':["JRR TOkien","Gustavo"],"publish_date":"1974","number_of_pages":"440","identifier_OL":"OL88899"}]
 
 # This function initialize home page
 # request: The request object used to generate this response.
@@ -13,9 +13,19 @@ def initHome(request):
     if request.method == 'POST':
         form = FormsBook(request.POST)
         if form.is_valid():
-            author = form.cleaned_data["book_name"]
-            book = form.cleaned_data["author_name"]
- 
+            author = form.cleaned_data["author_name"]
+            book = form.cleaned_data["book_name"]
+            check_author = form.cleaned_data["author_opt"]
+            check_book = form.cleaned_data["book_opt"]
+
+            if check_book == False and check_author == False:
+                messages.info(request, 'You have to select a type of search.')
+            else:
+                if book == "" and check_book == True:
+                    messages.info(request, 'Enter the name of a Book')
+                if author == "" and check_author == True:
+                    messages.info(request, 'Enter the name of an Author')
+            
             book_list = services.get_books_by_id()
             return redirect('Results')
     else:
